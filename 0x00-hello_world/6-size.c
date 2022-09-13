a9dd42f8377425a04b78de49687be1de2ab61cfb
Iroko <vibxtunx@gmail.com>
2022-09-12 18:55:53 -0700

    size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float;
+
+print("size of a chat: %lu byte(s)\n", (unsigned long)sizeof(d));
+print("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+print("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+print("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+print("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
