8f447a42ea0a84dfbabca007824de71b3c20230b
Iroko <vibxtunx@gmail.com>
2022-09-12 19:08:25 -0700

    size of datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@ int a;
 long int b;
 long long int c;
 char d;
-float;
+float f;
 
-print("size of a chat: %lu byte(s)\n", (unsigned long)sizeof(d));
-print("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-print("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-print("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-print("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+print("size of a chat: %d byte(s)\n", (unsigned long)sizeof(d));
+print("size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
+print("size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
+print("size of a long long int: %d byte(s)\n", (unsigned long)sizeof(d));
+print("size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
