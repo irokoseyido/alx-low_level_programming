c30f0e01ec8b4389690bf360dce93d35b3a21b5a
Iroko <vibxtunx@gmail.com>
2022-09-12 20:49:36 -0700

    using a write function to print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the strings in thr print function
+ *
+ * Description: using the main function
+ * this program prints and that piece of art is useful Dora Korpar, date
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
