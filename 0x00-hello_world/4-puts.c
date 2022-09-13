e60cbf11109a44ebaeea2a737d1c797e00d9db9e
Iroko <vibxtunx@gmail.com>
2022-09-12 19:31:06 -0700

    use put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * Description: main - entry point.
+ * main - print the string in the put function
  *
- * Return: 0 after printing the function.
+ * Description: using the main function
+ * Return: 0
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 	
 }
