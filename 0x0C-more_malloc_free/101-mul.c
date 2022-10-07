891295f7d833211d0431952d8cfb4ee39f63c1b3
Iroko <vibxtunx@gmail.com>
2022-10-07 14:52:12 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ int len = 0;
 while (*str++)
 len++;
 
-return (len)
+return (len);
 }
 
 /**
@@ -140,7 +140,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 int num, tens = 0;
 
 while (*(final_prod + 1))
-(final_prod + 1))
 final_prod++;
 
 while (*(next_prod + 1))
